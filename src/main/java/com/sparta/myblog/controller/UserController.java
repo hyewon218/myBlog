87ac9bbd90d0a4ab4af4a5d6eb7761ebdb1bd351
hyewon218 <won_11111@naver.com>
2023-06-30 01:02:18 +0900

    로그인 시 JWT 생성 및 쿠키에 저장 후 Response 객체에 추가, ResponseEntity 사용

--- src/main/java/com/sparta/myblog/controller/UserController.java
+++ src/main/java/com/sparta/myblog/controller/UserController.java
@@ -1,38 +1,47 @@
 package com.sparta.myblog.controller;
 
-import com.sparta.myblog.dto.*;
+import com.sparta.myblog.dto.ApiResponseDto;
+import com.sparta.myblog.dto.LoginRequestDto;
+import com.sparta.myblog.dto.SignupRequestDto;
+import com.sparta.myblog.jwt.JwtUtil;
 import com.sparta.myblog.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
 
-
-@Validated
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final JwtUtil jwtUtil;
 
-    // 회원 가입
+    // 회원가입
     @PostMapping("/signup")
-    public ApiResult signup(@Valid @RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<ApiResponseDto> signUp(@Valid @RequestBody SignupRequestDto requestDto) {
 
         userService.signup(requestDto);
 
-        return new ApiResult("회원가입 성공", HttpStatus.OK.value()); // 성공했다는 메시지, 상태코드 와 함께 Client 에 반환하기
+        return ResponseEntity.status(201).body(new ApiResponseDto("회원가입 성공", HttpStatus.CREATED.value()));
     }
 
     // 로그인
     @PostMapping("/login")
-    public ApiResult login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+    public ResponseEntity<ApiResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        userService.login(loginRequestDto);
 
-        userService.login(loginRequestDto, response);
+        //JWT 생성 및 쿠키에 저장 후 Response 객체에 추가
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(loginRequestDto.getUsername()));
 
-        return new ApiResult("로그인 성공", HttpStatus.OK.value()); // 로그인 성공시 ApiResult Dto 를 사용하여 성공메세지와 statusCode 를 띄움
+        return ResponseEntity.ok().body(new ApiResponseDto("로그인 성공", HttpStatus.CREATED.value()));
     }
 }
