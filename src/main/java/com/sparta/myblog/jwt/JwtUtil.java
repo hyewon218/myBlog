c194e08103a483b69a2fb99db0e128c40ebf4a73
hyewon218 <won_11111@naver.com>
2023-06-29 12:06:50 +0900

    토큰 검사 메서드 추가

--- src/main/java/com/sparta/myblog/jwt/JwtUtil.java
+++ src/main/java/com/sparta/myblog/jwt/JwtUtil.java
@@ -1,10 +1,12 @@
 package com.sparta.myblog.jwt;
 
-import com.sparta.myblog.entity.UserRoleEnum;
+import com.sparta.myblog.entity.User;
+import com.sparta.myblog.repository.UserRepository;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -16,6 +18,7 @@ import java.util.Date;
 
 @Slf4j(topic = "JwtUtil")
 @Component
+@RequiredArgsConstructor
 public class JwtUtil {
     // Header KEY 값
     public static final String AUTHORIZATION_HEADER = "Authorization"; // Cookie 의 Name 값
@@ -30,6 +33,7 @@ public class JwtUtil {
     private String secretKey;
     private Key key;
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+    private final UserRepository userRepository;
 
     @PostConstruct
     public void init() {
@@ -40,14 +44,12 @@ public class JwtUtil {
     }
 
     // 토큰 생성
-    public String createToken(String username, UserRoleEnum role) {
+    public String createToken(String username) {
         Date date = new Date();
 
         return BEARER_PREFIX + // Bearer 과 공백 추가로 붙여줌
                 Jwts.builder()
                         .setSubject(username) // 사용자 식별자값(ID)
-                        // 나중에 권한을 가져오고 싶다면 Key 값으로 claim 에서 꺼내서 사용할 수 있다.
-                        .claim(AUTHORIZATION_KEY, role) // 사용자 권한
                         // 생성되는 시점 기준으로 60분을 처리하기 위해
                         // date.getTime() : 현재 시간 + TOKEN_TIME : 60분
                         .setExpiration(new Date(date.getTime() + TOKEN_TIME)) // 만료 시간
@@ -56,13 +58,15 @@ public class JwtUtil {
                         .compact();
     }
 
-    // header 에서 JWT 가져오기
+    // Header 에서 JWT 가져오기
+    // HttpServletRequest 안에는 우리가 가져와야 할 토큰이 Header 에 들어있음
     public String getJwtFromHeader(HttpServletRequest request) {
         // bearer 붙은 토큰 값 가져온다.
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
         // 순수한 토큰을 한번에 Header 에서 뽑아온다.
+        // 코드가 있는지, BEARER 로 시작하는지 확인
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
-            return bearerToken.substring(7);
+            return bearerToken.substring(7); // 앞에 7글자를 지워줌 BEARER 가 6글자이고 한칸이 띄어져있기때문
         }
         return null;
     }
@@ -90,5 +94,28 @@ public class JwtUtil {
         // getBody() : Body 부분에 들어있는 Claims(데이터들이 들어있는 집합) 를 가지고 올 수 있다.
         // JWT : Claim 기반 Web Token
         return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public User checkToken(HttpServletRequest request) {
+
+        String token = this.getJwtFromHeader(request);
+        Claims claims;
+
+        User user = null;
+
+        if (token != null) {
+            if (this.validateToken(token)) {
+                // 토큰에서 사용자 정보 가져오기
+                claims = this.getUserInfoFromToken(token);
+
+            } else {
+                throw new IllegalArgumentException("존재하지 않는 토큰입니다.");
+            }
+            // 토큰에서 가져온 사용자 정보를 사용하여 DB 조회
+            user = userRepository.findByUsername(claims.getSubject()).orElseThrow(
+                    () -> new IllegalArgumentException("회원을 찾을 수 없습니다.")
+            );
+        }
+        return user;
     }
 }
