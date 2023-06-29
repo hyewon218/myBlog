c4653d5746971140d5b8e3af962ff1ded8e9cfa5
hyewon218 <won_11111@naver.com>
2023-06-29 12:07:49 +0900

    mapping 공통 경로 수정

--- src/main/java/com/sparta/myblog/controller/UserController.java
+++ src/main/java/com/sparta/myblog/controller/UserController.java
@@ -5,44 +5,34 @@ import com.sparta.myblog.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
-@Slf4j
+@Validated
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/api/auth")
 public class UserController {
 
     private final UserService userService;
 
     // 회원 가입
-    @PostMapping("/auth/signup")
-    public ApiResult signup(@Valid @RequestBody SignupRequestDto requestDto, BindingResult bindingResult) {
-        // Validation 예외처리
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        if(fieldErrors.size() > 0) {
-            for (FieldError fieldError : bindingResult.getFieldErrors()) {
-                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-            }
-        }
+    @PostMapping("/signup")
+    public ApiResult signup(@Valid @RequestBody SignupRequestDto requestDto) {
+
         userService.signup(requestDto);
 
-        // 성공했다는 메시지, 상태코드 와 함께 Client 에 반환하기
-        return new ApiResult("회원가입 성공", HttpStatus.OK.value());
+        return new ApiResult("회원가입 성공", HttpStatus.OK.value()); // 성공했다는 메시지, 상태코드 와 함께 Client 에 반환하기
     }
 
     // 로그인
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     public ApiResult login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
 
         userService.login(loginRequestDto, response);
 
-        return new ApiResult("로그인 성공", HttpStatus.OK.value()); // 로그인 성공시 ApiResult Dto 를 사용하여 성공메세지와 statusCode를 띄움
+        return new ApiResult("로그인 성공", HttpStatus.OK.value()); // 로그인 성공시 ApiResult Dto 를 사용하여 성공메세지와 statusCode 를 띄움
     }
 }
