b63872b2b518ea2ff1380ba4822ca67466427f4b
hyewon218 <won_11111@naver.com>
2023-07-10 21:15:59 +0900

    메인페이지 요청 허가

--- src/main/java/com/sparta/myblog/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/myblog/config/WebSecurityConfig.java
@@ -68,14 +68,15 @@ public class WebSecurityConfig {
         http.authorizeHttpRequests((authorizeHttpRequests) ->
                 authorizeHttpRequests
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() // resources 접근 허용 설정
-                        .requestMatchers("/api/user/**").permitAll() // '/api/auth/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
+                        .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers(HttpMethod.GET, "/api/posts").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/post/**").permitAll() // 'GET /api/post' 로 시작하는 요청 모두 접근 허가
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
         http.formLogin((formLogin) ->
                 formLogin
-                        .loginPage("/api/user/login-page").permitAll()
+                        .loginPage("/api/user/login-page").permitAll() // 로그인 페이지를 제공하는 URL 을 설정
         );
 
         // 필터 관리
