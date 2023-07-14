58308a349f0f7d05ba342aa061a7c5e75ea34ad7
hyewon218 <won_11111@naver.com>
2023-07-14 23:52:28 +0900

    메인 화면(게시글 전체 조회) 구현, 첫 화면에 보여지도록

--- src/main/java/com/sparta/myblog/controller/HomeController.java
+++ src/main/java/com/sparta/myblog/controller/HomeController.java
@@ -1,12 +1,29 @@
 package com.sparta.myblog.controller;
 
+import com.sparta.myblog.dto.PostResponseDto;
+import com.sparta.myblog.service.PostService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final PostService postService;
+
+    // 게시글 목록 전체 조회
     @GetMapping("/")
-    public String home() {
+    public String getPosts(Model model) {
+        log.info("게시글 전체 조회");
+        List<PostResponseDto> dto = postService.getPosts2();
+        model.addAttribute("posts", dto);
+        log.info(String.valueOf(dto.size())); // 게시글 갯수
         return "index";
     }
 }
