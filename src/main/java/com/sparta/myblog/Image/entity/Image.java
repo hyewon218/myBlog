f696c2445f1887b83685a07b1c2e6d6e1f32f572
hyewon218 <won_11111@naver.com>
2023-12-31 10:06:40 +0900

    Image 추가

---
+++ src/main/java/com/sparta/myblog/Image/entity/Image.java
@@ -0,0 +1,52 @@
+package com.sparta.myblog.Image.entity;
+
+import com.sparta.myblog.entity.ChatRoom;
+import com.sparta.myblog.entity.Post;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Getter
+@Table(name="Images")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="post_id")
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="chat_room_id")
+    private ChatRoom chatRoom;
+
+    public Image(Post post,String urlText){
+        this.post = post;
+        this.imageUrl = urlText;
+    }
+
+    public Image(ChatRoom chatRoom,String urlText){
+        this.chatRoom = chatRoom;
+        this.imageUrl = urlText;
+    }
+}
